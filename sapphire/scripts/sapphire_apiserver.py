aa3e52d7798241123c99af8c56146c379af80b28
JeremyB <jeremybillheimer@gmail.com>
2013-05-01 21:25:13 -0500

    Added API server run script

---
+++ sapphire/scripts/sapphire_apiserver.py
@@ -0,0 +1,18 @@
+#! python
+#
+# <license>
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# 
+# 
+# Copyright 2013 Sapphire Open Systems
+#  
+# </license>
+#
+
+from sapphire.apiserver.main import main
+
+if __name__ == "__main__":
+    main()
+
